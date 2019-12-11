db17543deaf20501185cdf3ac8de0dad0f729638
yong.fei <yong.fei@oriente.com>
2019-12-11 17:59:02 +0800

    add mysql

--- src/main/java/com/bestfei/springboot_basedemo/SpringbootBasedemoApplication.java
+++ src/main/java/com/bestfei/springboot_basedemo/SpringbootBasedemoApplication.java
@@ -1,11 +1,13 @@
 package com.bestfei.springboot_basedemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("com.bestfei.springboot_basedemo.mapper")
 public class SpringbootBasedemoApplication {
 
 	public static void main(String[] args) {
