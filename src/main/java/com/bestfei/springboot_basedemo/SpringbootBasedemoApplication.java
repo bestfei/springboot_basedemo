36c50ab74222b6c55d0369f73a946923ff52c88a
yong.fei <yong.fei@oriente.com>
2020-03-20 16:59:47 +0800

    add 分页查询

--- src/main/java/com/bestfei/springboot_basedemo/SpringbootBasedemoApplication.java
+++ src/main/java/com/bestfei/springboot_basedemo/SpringbootBasedemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@MapperScan("com.bestfei.springboot_basedemo.mapper")
+@MapperScan({"com.bestfei.springboot_basedemo.mapper","com.bestfei.springboot_basedemo.dao"})
 public class SpringbootBasedemoApplication {
 
 	public static void main(String[] args) {
